80158c54736bb6eeae549628a8bb8d9312d40702
Pulkit Gupta <pulkitgupta2k@gmail.com>
2020-05-19 22:55:45 +0530

    uploader update

--- helper_upload.py
+++ helper_upload.py
@@ -1,13 +1,42 @@
-from bs4 import BeautifulSoup
-import requests
-from pprint import pprint
-import json
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import os
+import glob
+import time
+from wproto_creds import *
 
-def getHTML(link):
-    with open("cookie_gpl.txt") as f:
-        cookie = f.readline()
-    headers = {'User-agent': 'Mozilla/5.0', 'Cookie': cookie, 'DNT': '1'}
-    req = requests.get(link, headers=headers)
-    html = req.content
-    print(html)
-    return html
+driver = webdriver.Chrome('./chromedriver')
+
+def login(user, passwd):
+    driver.get("https://vm1.wproto.net/admin/home/")
+    username = driver.find_element_by_id("login_name")
+    password = driver.find_element_by_id("passwd")
+    username.send_keys(user)
+    password.send_keys(passwd)
+    submit = driver.find_element_by_xpath("//button[@type='submit']")
+    submit.click()
+
+def upload_plugin(name):
+    driver.get("https://vm1.wproto.net/modules/wp-toolkit/index.php/index/plugins")
+    upload_plugin = driver.find_element_by_xpath("//button[@data-test-id='toolbar-upload-button']")
+    upload_plugin.click()
+    driver.find_element_by_xpath("//input[@type='file']").send_keys(os.getcwd()+"/plugins/{}".format(name))
+    time.sleep(2)
+    ok_button = driver.find_element_by_xpath("//button[@type='button' and @class='pul-button pul-button--lg pul-button--primary pul-button--active']")
+    ok_button.click()
+    while(True):
+        try:
+            driver.find_element_by_xpath("//input[@type='file']").is_displayed()
+        except:
+            break
+
+def upload_driver():
+    login(user, passwd)
+    name = []
+    names = glob.glob("plugins/*.zip")
+    for name in names:
+        name = name.replace("plugins\\", "")
+        print(name)
+        upload_plugin(name)
+
+upload_driver()
